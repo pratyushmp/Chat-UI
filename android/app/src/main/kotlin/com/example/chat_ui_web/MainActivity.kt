84654aae32898907c1daf390b71f8bc4e4dc625a
infiniteoverflow <aswingopinathan1871@gmail.com>
2021-03-04 17:38:34 +0530

    Added Web Files and Constants.dart

---
+++ android/app/src/main/kotlin/com/example/chat_ui_web/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chat_ui_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xffffffff);
+Color secondaryColor = Color(0xffd1e6e7);
+
+Color inactiveIconColor = Color(0xff9da5b0);
+Color sentMessageColor = Color(0xffd0d3e3);
+Color taggedMessageColor = Color(0xff13aa92);
+Color textGreenColor = Color(0xff208a7b);
+Color boldTextColor = Color(0xff091433);
+Color primaryPurple = Color(0xffdfe1f9);
+Color secondaryPurple = Color(0xff5a68df);
+Color primaryYellow = Color(0xfff4eacf);
+Color secondaryYellow = Color(0xffc4b07a);
+Color primaryRed = Color(0xffffe0da);
+Color secondaryRed = Color(0xffbe6e5f);
